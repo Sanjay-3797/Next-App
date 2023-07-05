66cfca14ed4dd91ef981bfbf938147c1b1755c3c
Sanjay-3797 <sanjayg.ac.in@gmail.com>
2023-07-05 18:37:47 +0530

    Final touch to About US page

--- pages/aboutus/aboutus.js
+++ pages/aboutus/aboutus.js
@@ -1,9 +1,19 @@
-import React from "react";
+import Link from "next/link";
+
+const details = [
+  { id: 1, name: "Yash", role: "Senior Developer" },
+  { id: 2, name: "Vaibhav", role: "Backend Developer" },
+  { id: 3, name: "Suresh", role: "Frontend Developer" },
+];
 
 const AboutUs = () => {
   return (
     <>
-      <h2>The About Page</h2>
+      {details.map((member) => (
+        <ul>
+          <Link href={"aboutus/1"}>{member.name}</Link>
+        </ul>
+      ))}
     </>
   );
 };
